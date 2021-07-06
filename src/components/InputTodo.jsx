a037d2f59655dfdfe9f23350bcb243696544bcf1
KT <72961887+KazukiTamaru@users.noreply.github.com>
2021-07-06 18:14:46 +0900

    改修２ 改修

--- src/components/InputTodo.jsx
+++ src/components/InputTodo.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 
-export const InputTodo = () => {
-  <div className="input-area">
-    <input
-      placeholder="TODOを入力"
-      value={todoText}
-      onChange={onChangeTodoText}
-    />
-    <button onClick={onClickAdd}>追加</button>
-  </div>;
+export const InputTodo = (props) => {
+  const { todoText, onChange, onClick } = props;
+  return (
+    <div className="input-area">
+      <input placeholder="TODOを入力" value={todoText} onChange={onChange} />
+      <button onClick={onClick}>追加</button>
+    </div>
+  );
 };
